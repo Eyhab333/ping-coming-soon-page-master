ea582e6f65620d9b6de579895ac5953328c1d733
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-02-09 11:50:54 +0300

    Validation

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-const variableName = document.getElementById("");
-const anotherVariable = document.getElementById("");
+// Elements
+let emailField = document.getElementById("email-field")
+let emailErrorMessage = document.getElementById("email-error-message")
+let submitButton = document.getElementById("submit-button")
 
-variableName.addEventListener("click", () => {
-  anotherVariable.classList.remove("");
-  variableName.style.display = "none";
+// Function: validate user's input 
+function validateEmail () {
+  // If user`s input doesn't match the symboles => display the error message & change the borders's color to pink
+  if (!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+
+    emailErrorMessage.innerHTML = "Please provide a valid email address";
+    emailField.style.borderColor = "#FF5466";
+    return false;
+  }
+  // If user`s input matches the symboles => don't display the error message & change the borders's color to green
+  emailErrorMessage.innerHTML = "";
+  emailField.style.borderColor = "green";
+  return true
+}
+
+// prevent the submit button from submitting data
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault()
 })
