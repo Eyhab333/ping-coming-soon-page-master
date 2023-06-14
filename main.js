309d5fe6e2d651444d4fcb4e21c7c2029a1bbcb4
Ahab <ehabahmedfadl@gmail.com>
2023-06-14 12:52:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// Elements
+
 let emailField = document.getElementById("email-field");
 let emailErrorMessage = document.getElementById("email-error-message");
 let submitButton = document.getElementById("submit-button");
 
-// Function: validate user's input
+
 function validateEmail() {
-  // If user`s input doesn't match the symboles => display the error message & change the borders's color to pink
+  
   if (
     emailField.value === ""
   ) {
@@ -17,7 +17,7 @@ function validateEmail() {
     emailField.style.borderColor = "#FF5466";
     
   }
-  // If user`s input matches the symboles => don't display the error message & change the borders's color to green
+  
   else {
     emailErrorMessage.innerHTML = "";
     emailField.style.borderColor = "green";
@@ -25,7 +25,7 @@ function validateEmail() {
   }
 }
 
-// prevent the submit button from submitting data
+
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   validateEmail();
